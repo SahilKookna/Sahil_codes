69e4462c7493d2c12394fa14a128e4a75221b945
SahilKookna <choudharysahilkookna@gmail.com>
2026-06-15 17:34:19 +0530

    Time: 4 ms (15.45%), Space: 14.3 MB (23.62%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,27 +1,43 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> array;
-         
-        
-        for(int i=0;i<n;i++){
-            int a=0;
-            for(int j=0;j<n;j++){
-                if(i!=j&&nums[i]==nums[j]){
-                    a++;
-                    break;
-                };
-            }
-            if(a==0){
-                int b=nums[i];
-                array.push_back(b);
-            }
-            
+        vector<int>ans;
+        int a=nums[0];
+        for(int i=1;i<nums.size();i++){
+            a=a^nums[i];
 
-           
         }
-        return array;
+        // find the rightmost set bit in a;
+        long long b=0;
+        int c=a;
+        while(c!=0){
+            if((c&1)==1){
+                break;
+            }
+            b++;
+            c=c>>1;
+        }
+        vector<int>n;
+        vector<int>m;
+        for(int i=0;i<nums.size();i++){
+            if((nums[i]&(1<<b))!=0){
+                n.push_back(nums[i]);
+            }
+            else{
+                m.push_back(nums[i]);
+            }
+        }
+        int x=0;
+        int y=0;
+        for(int i=0;i<n.size();i++){
+            x=x^n[i];
+        }
+        for(int i=0;i<m.size();i++){
+            y=y^m[i];
+        }
+
+        ans.push_back(x);
+        ans.push_back(y);
+        return ans;
     }
-    
 };
